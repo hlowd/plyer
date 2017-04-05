1b6eced26932aeb57a5b10dd6557678de364fc0e
hello <hello@teah.net>
2017-04-05 22:16:26 +0800

    -

---
+++ Win/Win.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Win
+{
+    partial class WinGenerate
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.dtpSet = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Enabled = false;
+            this.btnGenerate.Location = new System.Drawing.Point(83, 44);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(63, 34);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "生成";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // dtpSet
+            // 
+            this.dtpSet.Enabled = false;
+            this.dtpSet.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dtpSet.Location = new System.Drawing.Point(12, 12);
+            this.dtpSet.Name = "dtpSet";
+            this.dtpSet.Size = new System.Drawing.Size(134, 26);
+            this.dtpSet.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 44);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(63, 34);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // WinGenerate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(161, 93);
+            this.Controls.Add(this.dtpSet);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "WinGenerate";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.DateTimePicker dtpSet;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Win/Win.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Con;
+namespace Win
+{
+    public partial class WinGenerate : Form
+    {
+        public WinGenerate()
+        {
+            InitializeComponent();
+        }
+
+        private string getMd6Str(string path)
+        {
+            MD5 _md5Handler = MD5.Create();
+            // Convert the input string to a byte array and compute the hash.
+
+            byte[] data = _md5Handler.ComputeHash(File.ReadAllBytes(path));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+
+                byte tmp = (byte)((data[i] + i) % 255);
+
+                sBuilder.Append(tmp.ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(@".\MSVC60.dll"))
+            {
+                MessageBox.Show("准备好库函数！");
+                return;
+            }
+            DateTime tmp = DateTime.Parse(dtpSet.Value.ToLongDateString());
+            dtpSet.Value =tmp;
+            TestTime tt = new TestTime(tmp,tmp.AddMonths(1),getMd6Str(@".\MSVC60.dll"));
+            BinaryFormatter b = new BinaryFormatter();
+            FileStream f = File.Create(@".\Player.dll");
+            b.Serialize(f,tt);
+            f.Flush();
+            f.Close();
+            btnGenerate.Enabled = false;
+            dtpSet.Enabled = false;
+            MessageBox.Show("注册文件生成完毕！"); 
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnGenerate.Enabled = true;
+            dtpSet.Enabled = true;
+        }
+       
+    }
+
+}
